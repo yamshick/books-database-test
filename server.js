1b3046159d269a9414a516a2823018fcc5afeabe
username <myfancyemail@gmail.com>
2023-08-15 01:53:24 +0300

    test

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const port = 3000
 
 app.get('/', (req, res) => {
     try {
-        const rows = readDbSync(dbPath)
-        console.log(rows.length)
-        res.send(rows.map(row => row.genre).join('\n\n\n\n'))
-        // res.send('hello world')      
+        // const rows = readDbSync(dbPath)
+        // console.log(rows.length)
+        // res.send(rows.map(row => row.genre).join('\n\n\n\n'))
+        res.send('hello world')      
     } catch (e) {
         res.send(e.toString())
     }
