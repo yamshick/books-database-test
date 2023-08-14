244d97f405e463de3a4ac9684c066723a71cb6c1
username <myfancyemail@gmail.com>
2023-08-15 01:59:11 +0300

    express stuff

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+// require("dotenv").config();
+const app = express();
+var cors = require('cors')
+
+
+
+app.use(bodyParser.json());
+
+app.use(cors())
 const {readDB, readDbSync} = require('./database')
 
 const dbPath = './combat_fiction34.db'
 
-const express = require('express')
-const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     try {
-        // const rows = readDbSync(dbPath)
-        // console.log(rows.length)
-        // res.send(rows.map(row => row.genre).join('\n\n\n\n'))
-        res.send('hello world')      
+        const rows = readDbSync(dbPath)
+        console.log(rows.length)
+        res.send(rows.map(row => row.genre).join('\n\n\n\n'))
+        // res.send('hello world')      
     } catch (e) {
         res.send(e.toString())
     }
